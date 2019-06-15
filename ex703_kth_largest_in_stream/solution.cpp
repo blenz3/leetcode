026fafce96aba95922a72f4bbd7c61dda4d02e29
BradenLenz <lenzbradens@gmail.com>
2019-06-15 09:23:04 -0500

    Kth largest in stream w pq

---
+++ ex703_kth_largest_in_stream/solution.cpp
@@ -0,0 +1,31 @@
+class KthLargest {
+public:
+    KthLargest(int k, vector<int>& nums)
+        : k_(k)
+        , queue_()
+    {
+        for (auto num : nums)
+        {
+            add(num);
+        }   
+    }
+    
+    int add(int val) 
+    {
+        queue_.push(val);
+        if (queue_.size() > k_)
+            queue_.pop();
+        
+        return queue_.top();   
+    }
+    
+private:
+    int k_;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> queue_;
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
