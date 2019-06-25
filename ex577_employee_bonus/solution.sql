dac460d50e7872e6d09f04cfe83f6da8eab6d2d4
BradenLenz <lenzbradens@gmail.com>
2019-06-25 18:06:21 -0500

    Rando sql problems

---
+++ ex577_employee_bonus/solution.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+SELECT
+    e.name,
+    b.bonus
+FROM
+    Employee e LEFT JOIN Bonus b on e.empId = b.empId
+WHERE
+    b.bonus is NULL or b.bonus < 1000
