cba55e572fb856ac48733fb162d1a7a183e937ac
Braden Lenz <lenzbradens@gmail.com>
2019-08-28 21:48:23 -0700

    Create solution.js

---
+++ ex151_reverse_words/solution.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    return s.trim()
+        .split(' ')
+        .filter(word => word.length !== 0)
+        .reverse()
+        .join(' ');
+};
