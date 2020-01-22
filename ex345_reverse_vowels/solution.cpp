7bf6b83c1cbfd1eb716f045d519dc293fb263242
Braden Lenz <16659708+blenz3@users.noreply.github.com>
2020-01-22 13:50:57 -0800

    Update solution.cpp

--- ex345_reverse_vowels/solution.cpp
+++ ex345_reverse_vowels/solution.cpp
@@ -1,5 +1,39 @@
 class Solution {
 public:
+    // 2 ptrs with one at start and one at end and walk each until
+    // they cross swapping vowels as we go (O(n) time, O(1) space)
+    string reverseVowels(string s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+        
+        int startIdx = 0, endIdx = s.length() - 1;
+        while (startIdx < endIdx) {
+            while (startIdx < endIdx && !isVowel(s[startIdx])) {
+                startIdx++;
+            }
+            
+            while (startIdx < endIdx && !isVowel(s[endIdx])) {
+                endIdx--;
+            }
+            
+            if (!isVowel(s[startIdx]) || !isVowel(s[endIdx]))
+                break;
+            
+            auto tmp = s[startIdx];
+            s[startIdx] = s[endIdx];
+            s[endIdx] = tmp;
+            startIdx++;
+            endIdx--;
+        }
+        
+        return s;
+    }
+    
+#if 0
+    // Straight forward copy of each vowel into a stack
+    // and then swapping voewls with the top of the stack
+    // (O(n) time, O(n) space)
     string reverseVowels(string s) {
         std::stack<char> vowels;
         for (auto c : s) {
@@ -18,6 +52,7 @@ public:
         
         return s;
     }
+#endif
     
 private:
     bool isVowel(char c) {
