2799589220a81e5a34d9466a464205c5e7a63d62
Braden Lenz <16659708+blenz3@users.noreply.github.com>
2020-01-23 10:28:13 -0800

    Create solution.cpp

---
+++ ex235_lowest_common_ancestor/solution.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr || p == nullptr || q == nullptr) {
+            return nullptr;
+        }
+        
+        return findLowestCommonAncestor(root, p, q);
+    }
+    
+private:
+    
+    TreeNode* findLowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        TreeNode* lca = nullptr;
+        if (root == nullptr) {
+            return nullptr;
+        }
+        
+        bool nodesInSubtree = inSubtree(root, p) && inSubtree(root, q);
+        if (!nodesInSubtree) {
+            return lca;
+        } 
+            
+        lca = root;
+        TreeNode* left = findLowestCommonAncestor(root->left, p, q);
+        lca = (left == nullptr) ? lca : left;
+        
+        TreeNode* right = findLowestCommonAncestor(root->right, p, q);
+        lca = (right == nullptr) ? lca : right;
+        
+        return lca;
+    }
+    
+    bool inSubtree(TreeNode* root, TreeNode* target) {
+        if (root == nullptr) {
+            return false;
+        } else if (root == target) {
+            return true;
+        } 
+        
+        if (root->val > target->val) {
+            return inSubtree(root->left, target);
+        } else {
+            return inSubtree(root->right, target);
+        }
+    }
+};
