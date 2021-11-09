b26c8f75644a3d6a226b42cde12d2eb334a0b92e
Braden Lenz <16659708+blenz3@users.noreply.github.com>
2021-11-09 14:37:23 -0800

    Update solution.cpp

--- ex35_search_insert_position/solution.cpp
+++ ex35_search_insert_position/solution.cpp
@@ -1,14 +1,80 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target)
-    {
-        // linear time search, constant space. Trawl through the list until we find a larger value. Return that index
-        int i = 0;
-        while (i < nums.size() && nums[i] < target)
-        {
-            i++;
+    // Brute force solution (O(n) time, O(1) space
+    //  - Search across each element in the list. When you find the number return it. If you find a number
+    //    larger than the target then return the index
+    //
+    // Binary search solution(O(lgn)time, O(1) space
+    //  - Binary search across the space of the list. If a match is found return the match. Otherwise
+    //    determine the appropriate position for the target
+    
+    int searchInsert(vector<int>& nums, int target) {
+        // Brute force solution
+        // return linearSearchIndex(nums, target);
+
+        // Optimized solution
+        return binarySearchIndex(nums, target);
+        // if (nums.empty()) {
+        //     return 0;
+        // }
+        // return binarySearchIndexRec(nums, target, 0, nums.size() - 1);
+    }
+    
+    int linearSearchIndex(vector<int>& nums, int target) {
+        if (nums.empty()) {
+            return 0;
         }
         
-        return i;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == target || nums[i] > target) {
+                return i;
+            } 
+        }
+        
+        return nums.size();
+    }
+    
+    int binarySearchIndex(vector<int>& nums, int target) { 
+        // No elements - insert at 0
+        if (nums.empty()) {
+            return 0;
+        }
+        
+        int start = 0, end = nums.size() - 1;
+        while (start <= end) {
+            int midpoint = (start + end) / 2;
+            
+            // Exact match
+            if (nums[midpoint] == target) {
+                return midpoint;
+            } else if (target < nums[midpoint]) {
+                end = midpoint - 1;
+            } else {
+                start = midpoint + 1;
+            }
+        }
+        
+        return start;
+    }
+    
+    int binarySearchIndexRec(vector<int>& nums, int target, int start, int end) {
+        // Only one possibility left
+        if (start == end) {
+            // Either it is before or after this one
+            return (nums[start] < target) ? start + 1 : start;
+        } else if (start + 1 == end && nums[start] >= target) {
+            return start;
+        } else if (start + 1 == end && nums[end] >= target) {
+            return end;
+        }
+        
+        int midpoint = start + ((end - start + 1) / 2);
+        if (nums[midpoint] == target) {
+            return midpoint;
+        } else if (nums[midpoint] > target) {
+            return binarySearchIndexRec(nums, target, start, midpoint);
+        } else {
+            return binarySearchIndexRec(nums, target, midpoint, end);
+        }
     }
 };
